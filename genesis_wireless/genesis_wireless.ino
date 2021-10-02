f33d6ce8ecd0a8ea07c814e0d74c2d07fa05472a
HoaxParagon <59360325+HoaxParagon@users.noreply.github.com>
2021-10-02 15:37:30 -0600

    Update genesis_wireless.ino

--- genesis_wireless/genesis_wireless.ino
+++ genesis_wireless/genesis_wireless.ino
@@ -6,13 +6,15 @@
 
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  Serial.begin(115200);
+  while (!Serial){sleep 1;}
+  
   String MODE = "CONTROLLER";
   //String MODE = "BASE_UNIT";
   
-  //radio
-  RF24 radio (7, 8);
-
+  temp[][1] = {['a'], ['a'], ['a'], ['a'], ['a']};
+  RADIO_PAYLOAD_SIZE = sizeof(temp) // 5 characters
 
   // other pins
   // SCK 13
@@ -38,12 +40,22 @@ void setup() {
                             int SELECT = 9;
                             int START = 10;
                             };
+  typedef struct ButtonMessages{
 
+  };
+  // Let these addresses be used for the pair
+uint8_t address[][5] = {"base", "cont"};
 
   if (MODE == "CONTROLLER"){
     //struct controller = Controller;
     Controller controller = Controller;
     
+    //radio
+    RF24 radio (controller.RF24_CE, controller.RF24_CS);
+    radio.setPAlevel(RF24_PA_MED); // set to medium for testing
+    radio.setPayloadSize(RADIO_PAYLOAD_SIZE);
+    radio.openWritingPipe(1,address[0]);// reads from buttons, writes to base
+
     pinMode(controller.UP, INPUT_PULLUP);
     pinMode(controller.DOWN, INPUT_PULLUP);
     pinMode(controller.LEFT, INPUT_PULLUP);
@@ -52,12 +64,21 @@ void setup() {
     pinMode(controller.BUTTON_B, INPUT_PULLUP);
     pinMode(controller.BUTTON_C, INPUT_PULLUP);
     pinMode(controller.START, INPUT_PULLUP);
-    pinMode(controller.SELECT, INPUT_PULLUP);
+    //pinMode(controller.SELECT, INPUT_PULLUP); //not a real pin, used in base station
   
   // this is for the base station that plugs
   // into the sega
   else if (MODE=="BASE_UNIT"){
+    // struct
     Controller base = Controller;
+    
+    //radio
+    RF24 radio (controller.RF24_CE, controller.RF24_CS);
+    radio.setPAlevel(RF24_PA_MED); // set to medium for testing
+    radio.setPayloadSize(RADIO_PAYLOAD_SIZE):
+    radio.openReadingPipe(0, address[1]); //reads from controller, does stuff
+
+    // pin setups
     pinMode(base.UP, OUTPUT);
     pinMode(base.DOWN, OUTPUT);
     pinMode(base.LEFT, OUTPUT);
@@ -66,12 +87,22 @@ void setup() {
     pinMode(base.BUTTON_B, OUTPUT);
     pinMode(base.BUTTON_C, OUTPUT);
     pinMode(base.START, OUTPUT);
-    pinMode(base.SELECT, OUTPUT);
+    pinMode(base.SELECT, OUTPUT); // pulled high/low to use other alt pins
   }
 
-};
+  if (!radio.begin()){
+    Serial.println("radio hardware not responding...")
+    while(1){sleep 1;}
+  }
+}
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read from buttons,
+  // if buttons change,
+  // send changes
+
+  // read from radio stream
+  // if stream changes
+  // change button states
 
 }
